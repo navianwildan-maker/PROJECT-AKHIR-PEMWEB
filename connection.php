5cccb2aebe344f2899d02ba18e876594164b4996
Klava147 <123240109@student.upnyk.ac.id>
2025-11-09 12:31:01 +0700

    file php database

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$port = 3306;
+$username = "root";
+$password = "";
+$database = "rumahsakit";
+
+$connect = new mysqli($hostname, $username, $password, $database, $port);
+if ($connect->connect_error) {
+    die("Maaf koneksi gagal ".$connect->connect_error);
+} 
+?>
